e1c61486a2d1193a49574182d34a5186d51f4bed
MahmoudAdel1112 <whowanttolearn@gmail.com>
2025-08-11 04:49:24 +0300

    changeing environment variables

--- src/app/appwrite.js
+++ src/app/appwrite.js
@@ -3,8 +3,8 @@ import { Client, Account } from "appwrite";
 export const client = new Client();
 
 client
-  .setEndpoint("https://fra.cloud.appwrite.io/v1")
-  .setProject("68718bd1000edda8e81a"); // Replace with your project ID
+  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID)
+  .setProject(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT); // Replace with your project ID
 
 export const account = new Account(client);
 export { ID } from "appwrite";
